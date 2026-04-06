a570fda257ffd61741dea73b47f17be64c8bdb70
James <hanxiangli666@gmail.com>
2026-04-06 09:44:42 -0400

    proxy配置

--- vite.config.mjs
+++ vite.config.mjs
@@ -21,9 +21,17 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 31098,
+    proxy: {
+    '/api/sso': {
+      target: 'https://www.smartyihui.com',
+      changeOrigin: true,
+      onProxyReq(proxyReq) {
+        console.log('Proxy to:', proxyReq.path)
+      },
+    }
+    }
   },
 })
-
 
 //开发工具的配置文件，不是网站本身的代码。
 //Vite是一个构建工具，类似于一个"翻译机"——因为浏览器看不懂.vue文件，Vite负责把它翻译成浏览器能懂的普通HTML/CSS/JS。
